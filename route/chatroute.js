af91cc110e63e60a9557f62c0eded0b9e88837c6
yassin549 <khoualdiyassin26@gmail.com>
2020-12-11 17:30:00 +0500

    Added support for joining multiple chat rooms

--- route/chatroute.js
+++ route/chatroute.js
@@ -6,3 +6,9 @@ const router = express.Router();
 router.route("/").get(async (req, res, next) => {
   res.setHeader("Content-Type", "application/json");
   res.statusCode = 200;
+
+  let data = await Chats.find({ sender: "Anonymous" });
+  res.json(data)
+});
+
+module.exports = router;
