c3196718479eaac8d9e7c0897029d8fb76fcc6a0
yassin549 <khoualdiyassin26@gmail.com>
2020-12-14 16:15:00 +0500

    Integrated user typing indicator feature

---
+++ app.js
@@ -0,0 +1,59 @@
+//require the express module
+
+
+//bodyparser middleware
+app.use(express.json());
+
+//routes
+app.use("/chats", chatRouter);
+
+//set the express.static middleware
+app.use(express.static(__dirname + "/public"));
+
+//integrating socketio
+socket = io(http);
+
+
+
+//setup event listener
+socket.on("connection", socket => {
+
+  socket.on("disconnect", function () {
+    console.log("user disconnected");
+  });
+
+  //Someone is typing
+  socket.on("typing", data => {
+    socket.broadcast.emit("notifyTyping", {
+      user: data.user,
+      message: data.message
+    });
+  });
+
+  //when soemone stops typing
+  socket.on("stopTyping", () => {
+    socket.broadcast.emit("notifyStopTyping");
+  });
+
+  socket.on("chat message", async function (msg) {
+    //save chat to the database
+    let chatMessage = new Chat({ message: msg, sender: "Anonymous" });
+    await chatMessage.save();
+
+    //broadcast message to everyone in port:5000 except yourself.
+    socket.broadcast.emit("received", { message: msg });
+
+  });
+});
+
+http.listen(port, async () => {
+  connect
+    .then(() => {
+      console.log("Successfully connected to MongoDB");
+    })
+    .catch(err => {
+      console.error("Error connecting to MongoDB", err);
+    });
+
+  console.log("Running on Port: " + port);
+});
