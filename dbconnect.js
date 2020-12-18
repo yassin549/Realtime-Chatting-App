6fa5e572c5ea822145bb20fff55b157fea870c6f
yassin549 <khoualdiyassin26@gmail.com>
2020-12-18 09:15:00 +0500

    Added client side database connection

---
+++ dbconnect.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb://localhost:27017/chat";
+
+// Use native promises
+const connect = mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true // Recommended for newer versions
+
